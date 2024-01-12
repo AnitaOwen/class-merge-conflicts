edcb7a5086e68e3f35a46d1ccedf233dfaa72d82
Jennifer <jenniferpeterson@pursuit.org>
2024-01-12 13:50:17 -0500

    created a comment

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function createPerson(name, age) {
-    return {
-      name,
-      age,
-      friends: [],
-    };
-  }
-  
-  function addPersonAsFriend(personOne, friend) {
-    personOne.friends.push(friend);
-    return personOne;
-  }
-  
-  function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  return {
+    name,
+    age,
+    friends: [],
+  };
+}
+
+function addPersonAsFriend(personOne, friend) {
+  personOne.friends.push(friend);
+  return personOne;
+}
+
+function fn(...c) {
+  if (!c.every((a) => Number.isNumber(a)))
+    throw "All arguments must be numbers.";
+  return c.reduce((a, b) => a + b);
+}
+
+// hey there, my name is Jennifer and i am 6'2 🤓
+
+fn(1, 2, 3); //> 6
+fn(10, "B", 20); //> error All arguments must be numbers.
