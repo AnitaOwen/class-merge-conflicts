acc3254249f32fe55ae464dfeaf2a6ea6e116df4
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-12 13:52:29 -0500

    updated error message

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function createPerson(name, age) {
-    return {
-      name,
-      age,
-      friends: [],
-    };
-  }
-  
-  function addPersonAsFriend(personOne, friend) {
-    personOne.friends.push(friend);
-    return personOne;
-  }
-  
-  function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  return {
+    name,
+    age,
+    friends: [],
+  };
+}
+
+function addPersonAsFriend(personOne, friend) {
+  personOne.friends.push(friend);
+  return personOne;
+}
+
+function fn(...c) {
+  if (!c.every((a) => Number.isNumber(a)))
+    throw "All arguments must be numbers, please.";
+  return c.reduce((a, b) => a + b);
+}
+
+fn(1, 2, 3); //> 6
+fn(10, "B", 20); //> error All arguments must be numbers.
